447b4fdd5d73110ef0645bfdcc07f532a6b66217
Mattias Rydengren <mattias.rydengren@coderesque.com>
2011-06-19 14:01:11 +0200

    Updated the AssemblyInfo.cs file

--- src/Agdur/Properties/AssemblyInfo.cs
+++ src/Agdur/Properties/AssemblyInfo.cs
@@ -2,12 +2,9 @@
 using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
-[assembly: AssemblyTitle("Agdur 0.5")]
-[assembly: AssemblyDescription("Benchmarking for .NET")]
+[assembly: AssemblyTitle("Agdur")]
+[assembly: AssemblyDescription("Micro-benchmarking for the .NET framework")]
 [assembly: AssemblyCompany("The Agdur Project")]
 [assembly: AssemblyProduct("Agdur ")]
 [assembly: AssemblyCopyright("Copyright (C) The Agdur Project 2011")]
-[assembly: AssemblyVersion("0.5")]
-[assembly: AssemblyInformationalVersion("0.5")]
-[assembly: AssemblyFileVersion("0.5")]
 [assembly: AssemblyDelaySign(false)]
