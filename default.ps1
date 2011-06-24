f5d28dfe49d9d91583de5fc53bf6316f7e650675
Mattias Rydengren <mattias.rydengren@coderesque.com>
2011-06-24 17:37:45 +0200

    Made a few changes to the build file, including making the output of the build less verbose and updating the assembly info.

--- default.ps1
+++ default.ps1
@@ -21,38 +21,37 @@ task clean {
 }
 
 task build -depends clean {
-    New-Item $buildDirectory -ItemType Directory
+    New-Item $buildDirectory -ItemType Directory | Out-Null
 
     Generate-Assembly-Info `
         -file "$sourceDirectory\Agdur\Properties\AssemblyInfo.cs" `
         -title "Agdur $version" `
-        -description "Benchmarking for .NET" `
-        -company "The Agdur Project" `
+        -description "Micro-benchmarking for the .NET framework" `
         -product "Agdur" `
         -version $version `
-        -copyright "Copyright (C) The Agdur Project 2011" `
+        -copyright "Copyright (C) Mattias Rydengren 2011" `
 
-    msbuild $solution /p:OutDir=$buildDirectory /p:Configuration=Release
+    msbuild $solution /p:OutDir=$buildDirectory /p:Configuration=Release | Out-Null
 }
 
 task release -depends build {
-    New-Item $releaseDirectory -ItemType Directory
+    New-Item $releaseDirectory -ItemType Directory | Out-Null
 
     & $toolsDirectory\7-zip\7za.exe a $releaseDirectory\agdur-$version-release-net-4.0.zip `
         $buildDirectory\Agdur.dll `
         $buildDirectory\Agdur.pdb `
         $buildDirectory\Agdur.xml `
-        license.txt
+        license.txt | Out-Null
 }
 
 task nuget -depends release {
-    New-Item $nugetDirectory\lib\net40 -ItemType Directory
+    New-Item $nugetDirectory\lib\net40 -ItemType Directory | Out-Null
         
     Generate-NuGet-Spec `
         -file "$nugetDirectory\Agdur.nuspec" `
         -id "Agdur" `
         -version $version `
-        -description "Agdur is a simple benchmarking library for the .NET framework." `
+        -description "Agdur is a simple micro-benchmarking library for the .NET framework." `
         -author "Mattias Rydengren" `
         -licenseUrl "https://github.com/mrydengren/agdur/blob/master/license.txt" `
         -projectUrl "https://github.com/mrydengren/agdur" `
@@ -63,7 +62,6 @@ task nuget -depends release {
         $buildDirectory\Agdur.pdb, `
         $buildDirectory\Agdur.xml 
     
-    & nuget pack $nugetDirectory\Agdur.nuspec -o $nugetDirectory
-    
-    Remove-Item -Force -Recurse -ErrorAction SilentlyContinue -LiteralPath $nugetDirectory\lib, $nugetDirectory\Agdur.nuspec
+    & nuget pack $nugetDirectory\Agdur.nuspec -o $nugetDirectory | Out-Null
+    Remove-Item -LiteralPath $nugetDirectory\lib, $nugetDirectory\Agdur.nuspec -Force -Recurse -ErrorAction SilentlyContinue
 }
--- src/Agdur/Properties/AssemblyInfo.cs
+++ src/Agdur/Properties/AssemblyInfo.cs
@@ -3,10 +3,10 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 [assembly: AssemblyTitle("Agdur 0.6")]
-[assembly: AssemblyDescription("Benchmarking for .NET")]
-[assembly: AssemblyCompany("The Agdur Project")]
+[assembly: AssemblyDescription("Micro-benchmarking for the .NET framework")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Agdur ")]
-[assembly: AssemblyCopyright("Copyright (C) The Agdur Project 2011")]
+[assembly: AssemblyCopyright("Copyright (C) Mattias Rydengren 2011")]
 [assembly: AssemblyVersion("0.6")]
 [assembly: AssemblyInformationalVersion("0.6")]
 [assembly: AssemblyFileVersion("0.6")]
