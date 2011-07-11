53cdf7e146ba12b27ab3bf77a89e252f2a3ab09c
Mattias Rydengren <mattias.rydengren@coderesque.com>
2011-07-11 22:22:44 +0200

    Merged two classes with the same name in different namespaces.

--- src/Agdur/BenchmarkBuilderWithSyntaxExtensions.cs
+++ src/Agdur/BenchmarkBuilderWithSyntaxExtensions.cs
@@ -1,10 +1,32 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Agdur.Abstractions;
 
 namespace Agdur
 {
     public static class BenchmarkBuilderWithSyntaxExtensions
     {
+        /// <summary>
+        /// Specifies how the value of the custom metric should be calculated.
+        /// </summary>
+        /// <param name="name">The name of the custom metric.</param>
+        /// <param name="func">The metric.</param>
+        public static IBenchmarkBuilderInSyntax<IBenchmarkBuilderContinutation> WithCustom(this IBenchmarkBuilderWithSyntax<IBenchmarkBuilderContinutation> builder, string name, Func<IEnumerable<double>, double> func)
+        {
+            return builder.WithCustom(new SingleValueMetric(name, func));
+        }
+
+        /// <summary>
+        /// Specifies how the value of the custom metric should be calculated.
+        /// </summary>
+        /// <param name="name">The name of the custom metric.</param>
+        /// <param name="func">The metric.</param>
+        public static IBenchmarkBuilderInSyntax<IBenchmarkBuilderContinutation> WithCustom(this IBenchmarkBuilderWithSyntax<IBenchmarkBuilderContinutation> builder, string name, Func<IEnumerable<double>, IEnumerable<double>> func)
+        {
+            return builder.WithCustom(new MultipleValueMetric(name, func));
+        }
+
         /// <summary>
         /// Specifies that the average value should be calculated.
         /// </summary>
