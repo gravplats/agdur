4bdddd8e42d0c222e635d407bd2bf587957a9cd1
Mattias Rydengren <mattias.rydengren@coderesque.com>
2011-07-14 18:18:48 +0200

    Added documentation for the text generator.

--- src/Agdur/IO/TextGenerator.cs
+++ src/Agdur/IO/TextGenerator.cs
@@ -4,16 +4,29 @@ using Agdur.Introspection;
 
 namespace Agdur.IO
 {
+    /// <summary>
+    /// Provides functionality for generating output of a benchmark.
+    /// </summary>
     public class TextGenerator
     {
         private readonly IBenchmarkBuilderContinutation builder;
 
+        /// <summary>
+        /// Creates a new instance of the <see cref="TextGenerator"/> class.
+        /// </summary>
+        /// <param name="builder"></param>
         public TextGenerator(IBenchmarkBuilderContinutation builder)
         {
             Ensure.NotNull(builder, "builder");
             this.builder = builder;
         }
 
+        /// <summary>
+        /// Generates output using the specified writer and output strategy.
+        /// </summary>
+        /// <param name="writer">The writer that should be used to generate the output.</param>
+        /// <param name="outputStrategy">The output strategy that should be used to generate the output.</param>
+        /// <returns>The output of a benchmark.</returns>
         public string Generate(TextWriter writer, IOutputStrategy outputStrategy)
         {
             Ensure.NotNull(writer, "writer");
