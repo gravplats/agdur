9901d2a06bcdb334e2a253b6543d3ec036a21a9b
Mattias Rydengren <mattias.rydengren@coderesque.com>
2012-05-06 15:22:54 +0200

    Added comments to an interface.

--- src/Agdur/Abstractions/IBenchmarkBuilderToSyntax.cs
+++ src/Agdur/Abstractions/IBenchmarkBuilderToSyntax.cs
@@ -4,6 +4,10 @@ namespace Agdur.Abstractions
 {
     public interface IBenchmarkBuilderToSyntax
     {
+        /// <summary>
+        /// Writes output to the specified writer.
+        /// </summary>
+        /// <param name="writer">The writer that the output should be written to.</param>
         IBenchmarkBuilderAsSyntax ToCustom(TextWriter writer);
     }
 }
