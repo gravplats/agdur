6a7210841647184eedfb07f9eae762b60fbe4bb4
Mattias Rydengren <mattias.rydengren@coderesque.com>
2012-05-06 15:17:12 +0200

    Made visitor classes inner classes in output strategies.

--- src/Agdur/IO/FormattedOutputStrategy.cs
+++ src/Agdur/IO/FormattedOutputStrategy.cs
@@ -19,5 +19,41 @@ namespace Agdur.IO
             var visitor = new FormattedOutputMetricVisitor(writer);
             metrics.Accept(visitor);
         }
+
+        /// <summary>
+        /// Provides formatted output of the results of a metric.
+        /// </summary>
+        public class FormattedOutputMetricVisitor : IMetricVisitor
+        {
+            private readonly TextWriter writer;
+
+            /// <summary>
+            /// Creates a new instance of the <see cref="FormattedOutputMetricVisitor"/> class.
+            /// </summary>
+            /// <param name="writer">A writer</param>
+            public FormattedOutputMetricVisitor(TextWriter writer)
+            {
+                Ensure.ArgumentNotNull(writer, "writer");
+                this.writer = writer;
+            }
+
+            /// <inheritdoc/>
+            public void Visit(SingleValueMetric metric)
+            {
+                Ensure.ArgumentNotNull(metric, "metric");
+
+                string result = SingleValueFormatter.Output(metric.Name, metric.UnitOfMeasurement, metric.GetValue());
+                writer.WriteLine(result);
+            }
+
+            /// <inheritdoc/>
+            public void Visit(MultipleValueMetric metric)
+            {
+                Ensure.ArgumentNotNull(metric, "metric");
+
+                string result = MultipleValueFormatter.Output(metric.Name, metric.UnitOfMeasurement, metric.GetValues());
+                writer.WriteLine(result);
+            }
+        }
     }
 }
--- src/Agdur/IO/XmlOutputStrategy.cs
+++ src/Agdur/IO/XmlOutputStrategy.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Xml;
 using Agdur.Abstractions;
+using Agdur.Introspection;
 
 namespace Agdur.IO
 {
@@ -21,6 +22,49 @@ namespace Agdur.IO
                 metrics.Accept(visitor);
 
                 xmlWriter.WriteEndElement();
+            }
+        }
+
+        /// <summary>
+        /// Provides XML output of the results of a metric.
+        /// </summary>
+        public class XmlOutputMetricVisitor : IMetricVisitor
+        {
+            private readonly XmlWriter writer;
+
+            /// <summary>
+            /// Creates a new instance of the <see cref="XmlOutputMetricVisitor"/> class.
+            /// </summary>
+            /// <param name="writer"></param>
+            public XmlOutputMetricVisitor(XmlWriter writer)
+            {
+                Ensure.ArgumentNotNull(writer, "writer");
+                this.writer = writer;
+            }
+
+            /// <inheritdoc/>
+            public void Visit(SingleValueMetric metric)
+            {
+                Ensure.ArgumentNotNull(metric, "metric");
+                writer.WriteElementString(metric.Name, metric.GetValue());
+            }
+
+            /// <inheritdoc/>
+            public void Visit(MultipleValueMetric metric)
+            {
+                Ensure.ArgumentNotNull(metric, "metric");
+
+                var values = metric.GetValues();
+                for (int index = 0; index < values.Count; index++)
+                {
+                    writer.WriteStartElement(metric.Name);
+                    writer.WriteAttributeString("index", index.ToString());
+
+                    string value = values[index];
+                    writer.WriteString(value);
+
+                    writer.WriteEndElement();
+                }
             }
         }
     }
